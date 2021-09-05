33b1768d13f3daac9ded34fd19694c288f29fce2
Marvin Ren <marvin.ren@yale.edu>
2021-09-05 16:39:13 -0400

    clear question

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var questionsScreen = document.querySelector("#questions");
 var endScreen = document.querySelector("#end-screen");
 var feedback = document.querySelector ("#feedback");
 var timer = document.querySelector("#timer");
+var questionIndex = 0
 //start game with a score: time of 24
 var time = 50;
 //declare intervalId as a variable to use to call time 
@@ -12,69 +13,65 @@ var intervalId;
 var quizQuestions = [
     {
         question: "1. What number is one?",
-        choices: ["1", "2", "3", "4"],
+        choices: ["A", "2", "3", "4"],
         answer: "1",
     },
     {
         question:"2. What number is two?",
-        choiceOne: "1",
-        choiceTwo: "2",
-        choiceThree: "3",
-        choiceFour: "4",
+        choices: ["6", "2", "3", "4"],
         answer: "2"
     },
     {
         question:"3. What number is three?",
-        choiceOne: "1",
-        choiceTwo: "2",
-        choiceThree: "3",
-        choiceFour: "4",
+        choices: ["7", "2", "3", "4"],
         answer: "3",
     }
 ];
 
+var questionIndex = 0
 //function to start game
 var startQuiz = function() {
     startScreen.setAttribute("class", "hide");
     questionsScreen.removeAttribute("class", "hide");
-    // Loop over every question object
-for (let i = 0; i < quizQuestions.length; i++) {
-    //Display current question to user
-    currentQuestion = (quizQuestions[i].question);
-
-    //write the first question in h1 of questionsScreen
-    var questionName = document.createElement('h1');
-    questionName.textContent = (quizQuestions[i].question);
-    //append questionName to questionsScreen
-    questionsScreen.appendChild(questionName);
-
-    //write the first choice in questionsScreen
-    var questionList = document.createElement('ul');
-
-    //Choice #1
-    var questionListChoices = document.createElement('li');
-    questionListChoices.textContent = (quizQuestions[i].choiceOne);
-    questionList.appendChild(questionListChoices);
-    //Choice #2
-    
-    questionListChoices.textContent = (quizQuestions[i].choiceTwo);
-    
-    
-   //append questionList to questionsScreen
-    questionsScreen.appendChild(questionList);
-   
+    showQuestion();
     
 
-    //Compare answers
-    // if (
-    //     (questions.answer === 2)
-    // ) { time - 10;
-    //     alert("Wrong!");
-    // } else {
-    //     alert("Correct!");
-    // }
-};
+}
+//function to  showQuestions
+function showQuestion () {
 
+    // variable to store the HTML output, which includes the questions and answer choices 
+    var output = [];
+
+    // for each question...
+    
+        //Display current question to user
+        currentQuestion = (quizQuestions[questionIndex].question);
+        currentChoices = (quizQuestions[questionIndex].choices);
+    
+        //write the first question in h1 of questionsScreen
+        var questionName = document.createElement('h1');
+        questionName.textContent = (quizQuestions[questionIndex].question);
+        //append questionName to questionsScreen
+        questionsScreen.appendChild(questionName);
+
+        var questionList = document.createElement('ul');
+        for (var i = 0; i < currentChoices.length; i++) {
+            console.log(currentChoices[i])
+            var questionListChoices = document.createElement('li');
+            questionListChoices.textContent = currentChoices[i];
+            questionList.appendChild(questionListChoices);
+        }
+        questionsScreen.appendChild(questionList);
+        questionsScreen.addEventListener("click", verifyResponse)
+    };
+
+function verifyResponse () {
+    questionIndex++;
+    showQuestion();
+
+    }
+    
 //function to stop timer
 function endQuiz () {
     clearInterval(intervalId);
@@ -86,7 +83,6 @@ function endQuiz () {
             endQuiz();
         } 
     },1000);
-}
 
 //  //show total at the end
 // alert("You got " + time)
